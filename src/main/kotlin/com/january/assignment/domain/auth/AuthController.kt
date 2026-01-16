28aad0fd53b541eb47a42cc3db7dc048a2ca69ea
han16935 <han1693516@gmail.com>
2026-01-16 15:53:58 +0900

    refactor : 회원가입 validation annotation 작성

--- src/main/kotlin/com/january/assignment/domain/auth/AuthController.kt
+++ src/main/kotlin/com/january/assignment/domain/auth/AuthController.kt
@@ -3,9 +3,12 @@ package com.january.assignment.domain.auth
 import com.january.assignment.domain.auth.dto.JoinRequest
 import com.january.assignment.domain.auth.dto.JoinResponse
 import com.january.assignment.global.response.SuccessResponse
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus.CREATED
+import org.springframework.http.HttpStatus.OK
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,8 +18,9 @@ class AuthController(
     private val authService: AuthService
 ) {
 
-    @PostMapping("/login")
-    fun join(request : JoinRequest) : ResponseEntity<SuccessResponse<JoinResponse>> {
+    @PostMapping("/join")
+    fun join(@Valid @RequestBody request : JoinRequest) : ResponseEntity<SuccessResponse<JoinResponse>> {
         return ResponseEntity.status(CREATED).body(SuccessResponse.created(authService.join(request)))
     }
+
 }
