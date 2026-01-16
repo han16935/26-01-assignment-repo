8a33d29969a5bdebdf05f4bc2a4cfeb16371d300
han16935 <han1693516@gmail.com>
2026-01-16 16:43:02 +0900

    feat : Chat entity 작성

--- src/main/kotlin/com/january/assignment/domain/chat/Chat.kt
+++ src/main/kotlin/com/january/assignment/domain/chat/Chat.kt
@@ -1,15 +1,17 @@
 package com.january.assignment.domain.chat
 
+import com.january.assignment.domain.user.User
 import com.january.assignment.global.entity.BaseEntity
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
 class Chat (
     val question : String,
-    val answer : String
+    val answer : String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    val user : User
 ) : BaseEntity() {
 
     @Id
